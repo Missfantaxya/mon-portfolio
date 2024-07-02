fbfc6a0a628efff1ac23c1eff445a7e0f99d0d90
Alexia Skrz <alexia.sireuille@gmail.com>
2024-07-02 16:36:39 +0200

    add hover to ScrollToTop

--- src/components/molecules/ScrollToTop.jsx
+++ src/components/molecules/ScrollToTop.jsx
@@ -5,14 +5,14 @@ const ScrollToTop = () => {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
   }
-  // TODO ajouter un hover
+
   return (
     <div>
       <ButtonIcon
         Icon={IconArrowUp}
         onClick={scrollToTop}
         aria="Retour en haut"
-        style="fixed bottom-4 right-4 md:bottom-8 md:right-8 p-2 text-current rounded-full shadow-xl z-50"
+        style="fixed bottom-4 right-4 md:bottom-8 md:right-8 p-2 text-current hover:text-accent rounded-full shadow-xl z-50 "
       />
     </div>
   )
