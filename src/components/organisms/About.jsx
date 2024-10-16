6a5db32de595d0200b7a590e483aca5c73f0dde9
Alexia Skrz <alexia.sireuille@gmail.com>
2024-10-16 17:08:33 +0200

    About section mobile first WIP

--- src/components/organisms/About.jsx
+++ src/components/organisms/About.jsx
@@ -1,8 +1,31 @@
+// About.js
+
+import Image from 'next/image'
+import avatar from '@/../public/avatar.png'
+
 function About() {
   return (
-    <section id="about" className="h-dvh">
-      A propos
-    </section>
+    <div>
+      <div className="bg-primary overflow-hidden rounded-full w-max mx-auto">
+        <Image
+          className="mx-auto"
+          src={avatar}
+          width={150}
+          height={150}
+          alt="avatar"
+        />
+      </div>
+      <p>
+        En 2020, j'ai choisi de me reconvertir vers le développement web, un
+        domaine où l'innovation et l'adaptabilité sont essentielles.
+        Développeuse spécialisée en React.js et Next.js, j'aide les
+        auto-entrepreneurs et les TPE à créer des sites simples, efficaces et
+        accessibles. Mon approche pragmatique et humaine reflète mon désir de
+        rendre le numérique plus accessible à tous, tout en offrant des
+        solutions personnalisées qui répondent aux besoins concrets de mes
+        clients.
+      </p>
+    </div>
   )
 }
 
